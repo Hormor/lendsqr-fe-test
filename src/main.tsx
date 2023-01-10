dd2f2c1308613ed7088b5bbce063b5f2742fb998
Omolade Hassan <omoladehassan996@gmail.com>
2023-01-10 16:33:08 +0100

    feat: changed the files to typescript files

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import './index.scss'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from './components/Login';
+import DashboardNavbar from './components/dashboard/DashboardNavbar';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  // {
+  //   path: "/dashboard",
+  //   element: <Dashboard />,
+  // },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  <div className="main">
+    <RouterProvider router={router} />
     <App />
-  </React.StrictMode>,
+  </div>
 )
